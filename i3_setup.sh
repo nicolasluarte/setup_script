9b1694f415fd2ca40951ef9beb88e54815f38653
nicolasluarte <l.luarte@uc.cl>
2021-01-12 17:07:13 -0300

    i3 scripts creates symlink for picom configuration

--- i3_setup.sh
+++ i3_setup.sh
@@ -6,3 +6,5 @@ mkdir -p $HOME/.repos/i3
 git clone https://github.com/nicolasluarte/i3config.git $HOME/.repos/i3
 sudo rm $HOME/.config/i3/config
 ln -s $HOME/.repos/i3/config $HOME/.config/i3/config
+mkdir -p $HOME/.config/picom
+ln -s $HOME/.repos/i3/picom.conf $HOME/.config/picom/picom.conf
