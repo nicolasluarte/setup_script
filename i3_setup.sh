2af7cf7becb7672f653cf8a49df001ade73c5dc6
nicolasluarte <l.luarte@uc.cl>
2021-01-12 17:03:17 -0300

    i3 scripts install picom2

--- i3_setup.sh
+++ i3_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo pacman -Syyu
+sudo pacman -S picom
 mkdir -p $HOME/.repos/i3
 git clone https://github.com/nicolasluarte/i3config.git $HOME/.repos/i3
 sudo rm $HOME/.config/i3/config
