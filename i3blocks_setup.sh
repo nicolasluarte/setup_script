6aec7205ef8968daedee9d6f62496f4a735771da
nicolasluarte <l.luarte@uc.cl>
2021-02-02 21:26:45 -0300

    fixed mkdir in vim setup

--- i3blocks_setup.sh
+++ i3blocks_setup.sh
@@ -3,8 +3,8 @@
 sudo pacman -Syyu
 sudo pacman -S i3blocks
 sudo pacman -S noto-fonts-emoji
-yay -S acpi
-yay -S sysstat
+paru -S acpi
+paru -S sysstat
 sudo pacman -S python-requests
 sudo pacman -S lm_sensors
 git clone https://github.com/miklhh/i3blocks-config.git $HOME/.repos/i3blocks
--- vim_setup.sh
+++ vim_setup.sh
@@ -7,10 +7,10 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 curl -sL install-node.now.sh/lts | sudo bash
 mkdir -p $HOME/.repos/nvim
 git clone https://github.com/nicolasluarte/nvim-config.git $HOME/.repos/nvim
+mkdir -p $HOME/.config/nvim/plug-config
 ln -s $HOME/.repos/nvim/init.vim $HOME/.config/nvim/init.vim
-ln -s $HOME/.repos/nvim/plug-config $HOME/.config/nvim
+ln -s $HOME/.repos/nvim/plug-config/* $HOME/.config/nvim/plug-config
 pip3 install --user jedi
 sudo pacman -S ripgrep
-yay -S universal-ctags-git
 sudo pacman -S the_silver_searcher
 sudo pacman -S fd
